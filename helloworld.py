f754df0e2c6143822b313a4c566de2f963c0f50a
TintinPeng <2647206994@qq.com>
2020-07-30 18:51:42 +0800

    2020-7-30-6-51

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+'''
+@Author: your name
+@Date: 2020-07-29 15:17:32
+@LastEditTime: 2020-07-29 15:20:09
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /study_python/helloworld.py
+'''
+#!/usr/bin/python3
+print("Hello world!")
